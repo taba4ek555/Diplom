ee0b151593914cadd9c28d7ce2f8c34f149dc168
taba4ek555 <aleks.tabak15@gmail.com>
2025-04-05 22:31:35 +0300

    add epoch count range

--- pages/train.py
+++ pages/train.py
@@ -1,5 +1,5 @@
 import dash
-from dash import html
+from dash import html, dcc, callback, Input, State, Output
 import dash_bootstrap_components as dbc
 
 dash.register_page(__name__, path='/train')
@@ -7,6 +7,50 @@ dash.register_page(__name__, path='/train')
 layout = html.Div(
     [
         html.H1('Обучение нейросети'),
+        dbc.Form(
+            [
+                html.P(id='error', style={'color': 'red'}),
+                html.Div(id='classes-wrapper'),
+                dbc.Button(
+                    '+ Добавить класс',
+                    color='dark',
+                    id='add-class-button',
+                    n_clicks=0,
+                    style={'width': '100%'},
+                ),
+                dbc.Button(
+                    '+ Добавить слой',
+                    color='dark',
+                    id='add-layer-button',
+                    n_clicks=0,
+                    style={'width': '100%'},
+                ),
+                html.Label(id='epoch-count-label'),
+                dcc.Input(
+                    id='epoch-count-input',
+                    max=100,
+                    min=1,
+                    type='range',
+                    value=10,
+                    step=1,
+                ),
+                dbc.Button(
+                    'Обучить',
+                    id='train-button',
+                    n_clicks=0,
+                    style={'width': '100%'},
+                ),
+            ],
+            className='center test_form',
+        ),
     ],
     className='center',
 )
+
+
+@callback(
+    Output(component_id='epoch-count-label', component_property='children'),
+    Input('epoch-count-input', 'value'),
+)
+def show_epoch_count(val):
+    return f'Количество эпох: {val}'
